0e9be5d726a88805ab20f8a63488f044e98ca6fa
Bert Oosthuizen <bertoos@hotmail.com>
2021-06-25 22:42:06 +0200

    Add project files.

---
+++ FullMinerYield/Patches.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using System;
+using KMod;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+
+namespace FullMinerYield
+{
+    public class Patches : UserMod2
+    {
+        public static class Mod_OnLoad
+        {
+            public static void OnLoad()
+            {
+                Debug.Log("FullMinerYield MOD Loaded!");
+            }
+        }
+
+        [HarmonyPatch(typeof(WorldDamage), "OnDigComplete")]
+        internal class FullMinerYield_WorldDamage_OnDigComplete
+        {
+            private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+            {
+                IEnumerable<CodeInstruction> source = instr;
+                foreach (CodeInstruction codeInstruction in source.ToList<CodeInstruction>())
+                {
+                    if ((OpCode)codeInstruction.opcode == OpCodes.Ldc_R4 && (float)codeInstruction.operand == 0.5)
+                    {
+                        Debug.Log((object)" === Transpiler applied === ");
+                        //codeInstruction.operand = (OperandType)(ValueType) 1f;
+                        codeInstruction.operand = 1f;
+                    }
+                    yield return codeInstruction;
+                }
+            }
+        }
+    }
+}
